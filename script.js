844a54a08a8eb78c03bf714b9e5034f3a68c3c51
Pollenmiguel <pollenmiguel@gmail.com>
2023-10-06 03:56:31 +0200

    maj header, main , footer

--- script.js
+++ script.js
@@ -1,38 +1,26 @@
-const testimonialsContainer = document.querySelector('.testimonials');
-const prevBtn = document.getElementById('prev-btn');
-const nextBtn = document.getElementById('next-btn');
-
+const testimonials = document.querySelectorAll(".testimonial");
+const testimonialsContainer = document.querySelector(".testimonials");
 let currentIndex = 0;
-const testimonials = document.querySelectorAll('.testimonial');
 
-function showTestimonial(index) {
-    testimonials.forEach((testimonial, i) => {
-        testimonial.style.transform = `translateX(${100 * (i - index)}%)`;
+function scrollTestimonials() {
+    // Masquer tous les commentaires
+    testimonials.forEach((testimonial) => {
+        testimonial.style.display = "none";
     });
-}
 
-prevBtn.addEventListener('click', () => {
-    if (currentIndex > 0) {
-        currentIndex--;
-        showTestimonial(currentIndex);
-    }
-});
+    // Afficher le commentaire actuel
+    testimonials[currentIndex].style.display = "block";
 
-nextBtn.addEventListener('click', () => {
-    if (currentIndex < testimonials.length - 1) {
-        currentIndex++;
-        showTestimonial(currentIndex);
-    }
-});
-
-// Défilement automatique
-function autoScroll() {
-    if (currentIndex < testimonials.length - 1) {
-        currentIndex++;
-    } else {
+    currentIndex++;
+    
+    // Si on atteint la fin des commentaires, revenir au premier commentaire
+    if (currentIndex === testimonials.length) {
         currentIndex = 0;
     }
-    showTestimonial(currentIndex);
 }
 
-setInterval(autoScroll, 3000); // Défilement toutes les 3 secondes (3000 ms)
+// Appeler la fonction pour afficher le premier commentaire
+scrollTestimonials();
+
+// Définir l'intervalle pour faire défiler les commentaires
+setInterval(scrollTestimonials, 3000); // Défilement toutes les 3 secondes
