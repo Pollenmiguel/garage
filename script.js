5e4dbdcbddf9d64a4d7cb5161b56d58a345d8fda
Pollenmiguel <pollenmiguel@gmail.com>
2023-10-14 02:24:59 +0200

    maj page cars js

--- script.js
+++ script.js
@@ -24,3 +24,69 @@ scrollTestimonials();
 
 // Définir l'intervalle pour faire défiler les commentaires
 setInterval(scrollTestimonials, 3000); // Défilement toutes les 3 secondes
+
+
+
+
+
+// Récupérez les éléments du DOM
+const priceInput = document.getElementById('price');
+const mileageInput = document.getElementById('mileage');
+const yearInput = document.getElementById('year');
+const brandSelect = document.getElementById('brand');
+const resultsDiv = document.querySelector('.results');
+
+// Écoutez les événements de modification des filtres
+priceInput.addEventListener('input', applyFilters);
+mileageInput.addEventListener('input', applyFilters);
+yearInput.addEventListener('input', applyFilters);
+brandSelect.addEventListener('change', applyFilters);
+
+// Exemple de données de véhicules (à remplacer par vos données réelles)
+const vehicles = [
+    { brand: 'Toyota', price: 15000, mileage: 50000, year: 2018 },
+    { brand: 'Honda', price: 12000, mileage: 40000, year: 2019 },
+    // Ajoutez plus de données ici
+];
+
+// Fonction pour appliquer les filtres
+function applyFilters() {
+    const priceFilter = parseInt(priceInput.value) || 0;
+    const mileageFilter = parseInt(mileageInput.value) || 0;
+    const yearFilter = parseInt(yearInput.value) || 0;
+    const brandFilter = brandSelect.value;
+
+    const filteredVehicles = vehicles.filter(vehicle => {
+        return vehicle.price >= priceFilter &&
+            vehicle.mileage <= mileageFilter &&
+            vehicle.year >= yearFilter &&
+            (brandFilter === 'Tous' || vehicle.brand === brandFilter);
+    });
+
+    displayResults(filteredVehicles);
+}
+
+// Fonction pour afficher les résultats
+function displayResults(filteredVehicles) {
+    resultsDiv.innerHTML = '';
+
+    if (filteredVehicles.length === 0) {
+        resultsDiv.innerHTML = '<p>Aucun résultat trouvé.</p>';
+        return;
+    }
+
+    filteredVehicles.forEach(vehicle => {
+        const vehicleDiv = document.createElement('div');
+        vehicleDiv.classList.add('vehicle');
+        vehicleDiv.innerHTML = `
+            <h2>${vehicle.brand}</h2>
+            <p>Prix : ${vehicle.price} €</p>
+            <p>Kilométrage : ${vehicle.mileage} km</p>
+            <p>Année : ${vehicle.year}</p>
+        `;
+        resultsDiv.appendChild(vehicleDiv);
+    });
+}
+
+// Initialisez l'affichage initial
+displayResults(vehicles);
